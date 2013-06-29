9943f3b7a5353d672da2c77b60eecd273809d296
Caspar Yim <dawnyesky@foxmail.com>
2013-06-29 20:10:45 +0800

    整型转字符串添加进制默认参数

--- include/libyskalgrthms/util/string.h
+++ include/libyskalgrthms/util/string.h
@@ -9,7 +9,7 @@
 #define STRING_H_
 
 unsigned int ysk_atoi(char *character, unsigned int length);
-char* itoa(int integer, unsigned int radix);
-char* ivtoa(int* int_vec, unsigned int int_num, unsigned int radix);
+char* itoa(int integer, unsigned int radix = 10);
+char* ivtoa(int* int_vec, unsigned int int_num, unsigned int radix = 10);
 
 #endif /* STRING_H_ */
